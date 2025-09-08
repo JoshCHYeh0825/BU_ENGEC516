ec6bd1110d245117543260649269d9aa9bb5001f
Josh CH Yeh <jy0825@bu.edu>
2025-09-07 20:43:04 -0400

    Update q12.m

--- Assignment 1/q12.m
+++ Assignment 1/q12.m
@@ -1,4 +1,10 @@
+clc;
+clear all;
+close all;
+
+% Part a
 Fs = 1000;           % Sampling frequency (Hz), must be > 400 Hz
 T = 2;               % Signal duration (s)
 t = 0:1/fs:T-1/fs;   % Time vector
 x = cos(400*pi*t);   % Signal
+
