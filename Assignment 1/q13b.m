531c7b6c1635f494fd79a016af4e60780932570d
Josh CH Yeh <jy0825@bu.edu>
2025-09-08 15:24:45 -0400

    Finished q12

---
+++ Assignment 1/q13b.m
@@ -0,0 +1,28 @@
+clc;
+clear all;
+close all;
+
+% Part a
+T = 2;              % Period
+k = -6:6;           % Integer variables
+impulse = k * T;    % t = kT
+
+amp = ones(size(impulse));
+
+% Create a new figure and plot using stem
+figure;
+stem(impulse, amp, 'filled', 'LineWidth', 1.5, 'MarkerSize', 8);
+
+% --- Formatting for a clean look ---
+title('Impulse Train m(t) for T = 2', 'FontSize', 14);
+xlabel('Time (t)', 'FontSize', 12);
+ylabel('m(t)', 'FontSize', 12, 'Rotation', 0, 'HorizontalAlignment', 'right');
+grid on;
+ylim([0 1.2]);
+xlim([min(k)-1 max(k)+1]);
+
+% Move axes to the origin
+ax = gca; % Get current axes
+ax.XAxisLocation = 'origin';
+ax.YAxisLocation = 'origin';
+box off;
