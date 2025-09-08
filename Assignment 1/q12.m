35197ca6a06e3a4d688bfe86761dbf1d1653171a
Josh CH Yeh <jy0825@bu.edu>
2025-09-07 20:58:36 -0400

    plotted and 12a

--- Assignment 1/q12.m
+++ Assignment 1/q12.m
@@ -3,8 +3,29 @@ clear all;
 close all;
 
 % Part a
-Fs = 1000;           % Sampling frequency (Hz), must be > 400 Hz
-T = 2;               % Signal duration (s)
-t = 0:1/fs:T-1/fs;   % Time vector
-x = cos(400*pi*t);   % Signal
+% Define parameters for the signal
+T = 2;
+Fs = 1000;      % Sampling frequency, Hz
+dt = 1/Fs;      % Sampling Period, s
+t = -5:dt:5;    % Time vector
+N = T * Fs;     % Number of samples
+
+% Impulse Signal
+impulse = round(N/2);
+x = zeros(1, N);
+x(impulse) = 1;
+
+% Magnitude
+mag = abs(fft(x));
+
+% Frequency vector
+f = (0:N-1) * (Fs/N);
+
+% Plotting the Magnitude Spectrum
+plot(f, mag);
+title('Magnitude of FT of an Impulse');
+xlabel('Frequency (Hz)');
+ylabel('|X(jω)|');
+grid on;
+
 
